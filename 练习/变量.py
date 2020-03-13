62717234eb2f212a32efbc382892e874704e78db
Jacktting <62087174+Jacktting@users.noreply.github.com>
2020-03-13 21:05:58 +0800

    Add files via upload

---
+++ 练习/变量.py
@@ -0,0 +1,54 @@
+
+"""
+使用type()检查变量的类型
+"""
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'complex'>
+print(type(d)) # <class 'str'>
+print(type(e)) # <class 'bool'>
+"""
+使用input()函数获取键盘输入(字符串)
+使用int()函数将输入的字符串转换成整数
+使用print()函数输出带占位符的字符串
+"""
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d'%a)
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+"""
+赋值运算符和复合赋值运算符
+"""
+a = 10
+b = 3
+a += b # 相当于：a = a + b    13
+a *= a + 2 # 相当于：a = a * (a + 2) 13*15
+print(a)
+"""
+比较、逻辑和身份运算符的使用
+"""
+flag0 = 1 == 1   #1
+flag1 = 3 > 2   #1
+flag2 = 2 < 1   #0
+flag3 = flag1 and flag2 #0
+flag4 = flag1 or flag2  #1
+flag5 = not (1 != 2)    #0
+print('flag0 =', flag0) # flag0 = True
+print('flag1 =', flag1) # flag1 = True
+print('flag2 =', flag2) # flag2 = False
+print('flag3 =', flag3) # flag3 = False
+print('flag4 =', flag4) # flag4 = True
+print('flag5 =', flag5) # flag5 = False
+print(flag1 is True) # True
+print(flag2 is not False) # False
